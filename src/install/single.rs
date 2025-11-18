bf2e319d409737816723f5099307971445b0721a
Firstp1ck <firstpick1992@proton.me>
2025-11-18 19:21:32 +0100

    Remove unnecessary conditional compilation for build_install_command in single.rs

--- src/install/single.rs
+++ src/install/single.rs
@@ -5,7 +5,6 @@ use crate::state::PackageItem;
 #[cfg(not(target_os = "windows"))]
 use crate::state::Source;
 
-#[cfg(not(test))]
 use super::command::build_install_command;
 #[cfg(not(target_os = "windows"))]
 use super::logging::log_installed;
